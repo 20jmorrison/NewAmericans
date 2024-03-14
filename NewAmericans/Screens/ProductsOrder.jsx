67d780a5ca7e40feabe2502f5a545ed3b75f6500
Ben Levine <benalevine32@gmail.com>
2024-03-14 13:49:38 -0400

    Backend for Various Things

--- NewAmericans/Screens/ProductsOrder.jsx
+++ NewAmericans/Screens/ProductsOrder.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { fetchProducts } from '../components/Products/ProductFetching';
 
 const ProductsOrder = () => {
   const route = useRoute();
   const { categoryId } = route.params;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchProducts(categoryId);
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchData();
+  }, [categoryId]);
 
   return (
     <View style={styles.container}>
       <Text>Products Order Screen</Text>
-      <Text>Category ID: {categoryId}</Text> {/* Displaying the received category ID */}
+      <Text>Category ID: {categoryId}</Text>
+      <Text>Products:</Text>
+      <View>
+        {products.map(product => (
+          <Text key={product.id}>{product.name}</Text>
+        ))}
+      </View>
     </View>
   );
 };
