18aa13c407399993828c2145b59b7db1a115ac96
Jared Morrison <jrdmrrsn@gmail.com>
2024-03-14 12:58:24 -0400

    Set up front end requests for items. Waiting on backend to finish displaying items.

--- NewAmericans/Screens/ProductsOrder.jsx
+++ NewAmericans/Screens/ProductsOrder.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { fetchItems } from '../components/Categories/FetchingItems';
 
 const ProductsOrder = ({ route }) => {
   const { categoryId } = route.params;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Fetch categories
+        const itemsData = await fetchItems();
+        setItems(itemsData);
+      } catch (error) {
+        console.error('Error fetching itemsData:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   // Now you have access to the categoryId passed from the previous screen
   return (
