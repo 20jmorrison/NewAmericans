4c5fc73b1f4d3815d2f69e3e326f3c132de127e5
Ben Levine <benalevine32@gmail.com>
2024-03-19 23:33:57 -0400

    Report Infor

---
+++ NewAmericans/components/Reports/FetchingReportInfo.jsx
@@ -0,0 +1,18 @@
+const fetchReportData = async () => {
+    try {
+      const response = await fetch('http://50.187.63.220:3000/ReportData');
+      if (response.ok) {
+        const data = await response.json();
+        return data || []; // Ensure we have a fallback for empty or missing data
+      } else {
+        console.error('Failed to fetch student data');
+        return [];
+      }
+    } catch (error) {
+      console.error('Error fetching student data:', error);
+      return [];
+    }
+  };
+  
+  export { fetchReportData };
+  
