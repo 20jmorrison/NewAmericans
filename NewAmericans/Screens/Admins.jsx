da88768cc2ca36df9af461690ce94e324b8c7b68
Jared Morrison <jrdmrrsn@gmail.com>
2024-03-20 00:36:26 -0400

    Superuser password connected to back end

--- NewAmericans/Screens/Admins.jsx
+++ NewAmericans/Screens/Admins.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Keyboard } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Keyboard } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { fetchSuperuser } from '../components/Admins/FetchingSuperuser';
 
 const Admins = () => {
   const [superuserPasscode, setSuperuserPasscode] = useState('');
+  const [fetchedPasscode, setFetchedPasscode] = useState('');
   const navigation = useNavigation();
 
   const handleCheckPasscode = () => {
-    const predefinedPasscode = '1234';
-    if (superuserPasscode === predefinedPasscode) {
+    if (superuserPasscode == fetchedPasscode) {
       setSuperuserPasscode('');
       navigation.navigate('PasswordProtectedScreen');
     } else {
       console.log('Incorrect passcode');
+      console.log(fetchedPasscode);
+      console.log(superuserPasscode);
     }
   };
 
@@ -20,6 +23,19 @@ const Admins = () => {
     console.log('Done pressed');
     Keyboard.dismiss();
   };
+
+  useEffect(() => {
+    const getRealPasscode = async () => {
+      try {
+        const superuser = await fetchSuperuser();
+        setFetchedPasscode(superuser[0].passcode);
+      } catch (error) {
+        console.error('Error fetching real passcode:', error);
+      }
+    };
+
+    getRealPasscode();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -29,10 +45,12 @@ const Admins = () => {
         placeholder="****"
         value={superuserPasscode}
         onChangeText={(text) => setSuperuserPasscode(text)}
+        keyboardType="numeric"
         secureTextEntry={true}
-        keyboardType='numeric'
       />
-      <Button title="Submit" onPress={handleCheckPasscode} color="black" />
+      <TouchableOpacity style={styles.button} onPress={handleCheckPasscode}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
       <TouchableOpacity style={styles.button} onPress={handleDonePress}>
         <Text style={styles.buttonText}>Done</Text>
       </TouchableOpacity>
@@ -56,8 +74,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
-    width: '50%',
-    textAlign: 'center',
+    width: 200, // Adjust the width as needed
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
--- NewAmericans/Screens/PasswordProtectedScreen.jsx
+++ NewAmericans/Screens/PasswordProtectedScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { fetch } from '../components/Students/StudentFetching';
+
 
 const PasswordProtectedScreen = () => {
   return (
