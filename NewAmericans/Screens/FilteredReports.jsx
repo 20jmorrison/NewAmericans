0f54b92a343e5365c57f4e22993b9f3a3fc6ad9c
Jared Morrison <jrdmrrsn@gmail.com>
2024-04-08 15:03:06 -0400

    Final restyle of report screen

--- NewAmericans/Screens/FilteredReports.jsx
+++ NewAmericans/Screens/FilteredReports.jsx
@@ -19,7 +19,7 @@ const FilteredReports = ({ route }) => {
     try {
       // Create CSV data
       const csvData = createCSV(filteredData);
-      
+
       // Create file in cache directory
       console.log(FileSystem.documentDirectory);
       const fileUri = FileSystem.documentDirectory + 'exported_report.csv';
@@ -36,13 +36,13 @@ const FilteredReports = ({ route }) => {
     <View style={styles.container}>
       <ScrollView style={styles.scrollContainer}>
         <View style={styles.dataContainer}>
-          <Text style={styles.dataTitle}>Filtered</Text>
+          <Text style={styles.dataTitle}>Filtered Transactions</Text>
           <TouchableOpacity onPress={handleExport} style={styles.button}>
             <Text style={styles.buttonText}>Export Report</Text>
           </TouchableOpacity>
           {filteredData.map((item, index) => (
             <View key={index} style={styles.itemContainer}>
-              <Text>{item.Student_FirstName} {item.Student_LastName}: {item.ProductName} ({item.Quantity}) </Text>
+              <Text style={{ fontFamily: 'Nunito-Bold' }}>{item.Student_FirstName} {item.Student_LastName}: {item.ProductName} ({item.Quantity}) </Text>
               <Text>{formatDate(item.DateCreated)}</Text>
               <Text>Admin: {item.Admin_FirstName} {item.Admin_LastName}</Text>
             </View>
@@ -58,7 +58,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
   },
   scrollContainer: {
     flex: 1,
@@ -71,17 +70,35 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
+    fontFamily: 'Nunito-Black',
   },
   itemContainer: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    padding: 10,
-    marginBottom: 10,
+    marginTop: 20,
+
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 7,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 4,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+
   },
   button: {
     backgroundColor: '#F3D014',
     padding: 10,
     borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Nunito-Bold',
   },
 });
 
